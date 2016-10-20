a445d931f280ba3874e5f8ca45d414a93a0094d6
Dmitriy Kuragin <kuragin@speaktoit.com>
2016-10-20 15:58:55 +0700

    Adding pages

---
+++ app.js
@@ -0,0 +1,25 @@
+(function() {
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition || null;
+
+    function check_asr_availability() {
+        alert(SpeechRecognition);
+    }
+
+    function recognize() {
+        var recognition = new SpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = true;
+
+        recognition.onstart = function() { console.log("onstart"); }
+        recognition.onresult = function(event) { console.log("onresult"); }
+        recognition.onerror = function(event) { console.log(event); }
+        recognition.onend = function() { console.log("onend"); }
+
+        // alert(recognition);
+
+        recognition.start();
+    }
+
+    window.check_asr_availability = check_asr_availability;
+    window.recognize = recognize;
+})();
